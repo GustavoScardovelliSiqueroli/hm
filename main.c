09949f9549cd532fbdcd8066365695220c791f5b
gustavo <gustavosecardovelli@gmail.com>
2026-01-14 17:25:09 -0300

    feat: dump tests on main

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "dice.h"
 #include "enemy.h"
 #include "entity.h"
 #include "player.h"
@@ -6,42 +7,48 @@
 #include <time.h>
 #include <unistd.h>
 
-void clear() { system("clear"); }
-
 void print_waiting_int(char *pre_string, int (*funcao)()) {
   struct timespec t = {
-      .tv_nsec = 200000000,
+      .tv_nsec = 800000000,
   };
   char wait_dots[4] = "...";
 
   for (int i = 2; i >= 0; i--) {
-    clear();
-    printf("\n%s: %s\n", pre_string, wait_dots);
+    printf("\r%s: %s", pre_string, wait_dots);
+    fflush(stdout);
     nanosleep(&t, NULL);
-    wait_dots[i] = '\0';
+    wait_dots[i] = ' ';
   }
-  clear();
   int result = funcao();
-  printf("\n%s: %d\n", pre_string, result);
+  printf("\r%s: %d", pre_string, result);
+  fflush(stdout);
   nanosleep(&t, NULL);
 }
 
 int main(int argc, char *argv[]) {
   srand(time(NULL));
-  clear();
+  printf("******************************\n");
 
+  char p_name[50];
+
+  printf("nome: ");
+  scanf("%s", p_name);
   Player *p1 = player_create();
+  player_print(p1);
 
-  entity_take_damage(&p1->base, 80);
-  printf("Vida do player apos tomar dano: %.1f\n", p1->base.health);
-
-  Enemy e1;
-  enemy_init_random(&e1, 0, 1);
-
-  printf("Inimigo selecioado: %s\n", e1.name);
+  // Player *p1 = player_create();
+  //
+  // entity_take_damage(&p1->base, 80);
+  // printf("Vida do player apos tomar dano: %.1f\n", p1->base.health);
+  //
+  // Enemy e1;
+  // enemy_init_random(&e1, 0, 1);
+  //
+  // printf("Inimigo selecioado: %s\n", e1.name);
 
   // Vai cair contra um inimigo aleatorio dentro de alguns predefinidos,
   // depois rodar o dado para descobrir quanto de dano vai dar / tomar
 
+  printf("\n");
   return EXIT_SUCCESS;
 }
